16854befd2eba43faa0dd0641889d8d5faa8526c
Manuela Gomez <manu.gomez.e@gmail.com>
2024-04-14 22:41:16 -0500

    focus fix on schedule days for desktop

--- DanseAcademy/src/components/DaysForDesktop.jsx
+++ DanseAcademy/src/components/DaysForDesktop.jsx
@@ -6,7 +6,7 @@ export default function DaysForDesktop({ daysSpanish, setCurrentDay, currentDay 
     button:`z-20 ring-0
             rounded-tl-2xl rounded-br-2xl px-5 py-2
             transition-all ease-in font-bold
-            [&>p]:text-yellow-dark [&>p]:hover:text-gray-dark [&>p]:focus:text-gray-dark
+            [&>p]:text-yellow-dark [&>p]:hover:text-gray-dark [&>p]:focus:text-gray-dark focus:bg-yellow-dark
             hover:scale-110  hover:bg-yellow-dark`,
 
     daysDesktop: `mb-16 hidden justify-center 
